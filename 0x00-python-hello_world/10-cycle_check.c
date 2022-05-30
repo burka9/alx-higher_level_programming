66d3223be16bae25eed22d0440c9a5d0bcc44020
Biruk Ephrem <birukeph@gmail.com>
2022-05-30 23:50:14 +0300

    task

--- 0x00-python-hello_world/10-cycle_check.c
+++ 0x00-python-hello_world/10-cycle_check.c
@@ -1,31 +1,25 @@
 #include "lists.h"
 
 /**
- * check_cycle - check for loop in LL
- * @list: head of linked list
- *
- * Description - check for loops in LL
- * Return: 1 if cycled, 0 if not
- */
-
+* check_cycle - checks if singly linked list has a cycle in it
+* @list: linked list
+*
+* Return: 0 if there is no cycle, 1 if there is a cycle
+*/
 int check_cycle(listint_t *list)
 {
-	listint_t *slow, *fast;
+	listint_t *current = malloc(sizeof(listint_t));
 
-	if (!list)
+	current = list;
+	while (current != NULL)
 	{
-		return (0);
-	}
-	slow = list;
-	fast = list->next;
-	while (fast && slow && fast->next)
-	{
-		if (slow == fast)
+		if (current->next == list)
 		{
+			free(current);
 			return (1);
 		}
-		slow = slow->next;
-		fast = fast->next->next;
+		current = current->next;
 	}
+	free(current);
 	return (0);
 }
--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-str = 'The Zen of Python, by Tim Peterssssssssss'
-print("{}".format(str[:32]))
+import this
