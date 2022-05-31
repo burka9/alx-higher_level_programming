71e54381370905caa46a89c44e4d64cbb97912ac
Biruk Ephrem <birukeph@gmail.com>
2022-05-31 15:21:13 +0300

    task

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i+1, 10):
-        if (i is 8 and j is 9):
+        if (i == 8 and j == 9):
             print("{}{}".format(i, j), end="")
         elif (i is not j):
             print("{}{}".format(i, j), end=", ")
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def isLower(c):
-    return ord(c) > 96
+    return ord(c) > 96 and ord(c) <= 122
