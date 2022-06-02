afe4de3494d4f60b5ad7f80f3afc64ebbba98bb9
Biruk Ephrem <birukeph@gmail.com>
2022-06-02 23:48:33 +0300

    did a task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,16 +8,19 @@ if __name__ == '__main__':
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
 
+    a = int(argv[1])
+    b = int(argv[3])
+
     if argv[2] == '+':
-        result = add(int(argv[1]), int(argv[3]))
+        result = add(a, b)
     elif argv[2] == '-':
-        result = sub(int(argv[1]), int(argv[3]))
+        result = sub(a, b)
     elif argv[2] == '*':
-        result = mul(int(argv[1]), int(argv[3]))
+        result = mul(a, b)
     elif argv[2] == '/':
-        result = div(int(argv[1]), int(argv[3]))
+        result = div(a, b)
     else:
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
 
-    print('{} {} {} = {}'.format(argv[1], argv[2], argv[3], result))
+    print('{} {} {} = {}'.format(a, argv[2], b, result))
