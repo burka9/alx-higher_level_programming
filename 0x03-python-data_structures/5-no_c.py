036e89ad61eb6814c9fe539b004deec1a5c47785
Biruk Ephrem <birukeph@gmail.com>
2022-06-05 21:16:13 +0300

    did a task

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/bin/usr/python3
+def no_c(my_string):
+    temp = ''
+    for i in range(len(my_string)):
+        if my_string[i] is not 'c' or my_string[i] is not 'C':
+            temp += my_string[i]
+    return temp
