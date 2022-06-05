2565e632ecc1d09f0741382dd9245975a4be244e
Biruk Ephrem <birukeph@gmail.com>
2022-06-05 22:55:40 +0300

    did a task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    sum_a = (tuple_a[0] if len(tuple_a) > 0 else 0) + (tuple_b[0] if len(tuple_b) > 0 else 0)
-    sum_b = (tuple_a[1] if len(tuple_a) > 1 else 0) + (tuple_b[1] if len(tuple_b) > 1 else 0)
-    return (sum_a, sum_b)
+    sum_a = tuple_a + (0, 0)
+    sum_b = tuple_b + (0, 0)
+    return (sum_a[0] + sum_b[0], sum_a[1] + sum_b[1])
