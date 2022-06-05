0d7cfb4f9f86dd8287eded0f8a9d42ac65a4138d
Biruk Ephrem <birukeph@gmail.com>
2022-06-05 21:10:27 +0300

    did a task

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list.copy()
-    else:
-        return (__import__('3-replace_in_list').replace_in_list(my_list.copy(), idx, element)).copy()
+    cp = my_list.copy()
+    if idx >= 0 and idx < len(my_list):
+        cp[idx] = element
+    return cp
