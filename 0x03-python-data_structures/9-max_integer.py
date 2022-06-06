b68cd2d86523e3bf1025eaffd7bb3376ce076e99
Biruk Ephrem <birukeph@gmail.com>
2022-06-06 12:27:25 +0300

    did a task

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,9 @@
-from json.encoder import INFINITY
-from re import I
-
-
+#!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        max = -INFINITY
+        max = my_list[0]
         for i in my_list:
             if i > max:
                 max = i
