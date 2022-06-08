e17c8a17bec9efb6ef26b3d0665b798b9fd8cde5
Biruk Ephrem <birukeph@gmail.com>
2022-06-08 20:41:42 +0300

    python

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -9,7 +9,7 @@ def best_score(a_dictionary):
     keys = list(a_dictionary.keys())
 
     my_key = ''
-    num = a_dictionary[my_key]
+    num = a_dictionary[key[0]]
 
     for key in keys:
         if a_dictionary[key] >= num:
