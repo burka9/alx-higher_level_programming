bd1e334126f7c27d342e72f53e9010b28d17ea53
Biruk Ephrem <birukeph@gmail.com>
2022-06-08 14:24:11 +0300

    task done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary == None:
         return None
 
-    temp = list(a_dictionary.popitem())
+    temp = ['none', -1]
 
     for key in a_dictionary:
         if a_dictionary[key] > temp[1]:
