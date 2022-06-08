710dc49fa836fc1f6c48d5e836572f443f587cc9
Biruk Ephrem <birukeph@gmail.com>
2022-06-08 23:19:48 +0300

    python

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    list = {
-        'M': 1000,
-        'D': 500,
-        'C': 100,
-        'L': 50,
-        'X': 10,
-        'V': 5,
-        'I': 1
-    }
+    list = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     i = 0
     sum = 0
-    while i < len(roman_string):
-        if i + 1 < len(roman_string) and list[roman_string[i]] < list[roman_string[i + 1]]:
-            sum += list[roman_string[i + 1]] - list[roman_string[i]]
-            i += 2
-        else:
-            sum += list[roman_string[i]]
+    if isinstance(roman_string, str):
+        for i in range(len(roman_string) - 1):
+            if list[roman_string[i]] >= list[roman_string[i + 1]]:
+                sum += list[roman_string[i]]
+            else:
+                sum -= list[roman_string[i]]
             i += 1
-    return sum
+        sum += list[roman_string[i]]
+        return sum
+    return 0
