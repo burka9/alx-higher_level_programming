458dbe798ebed762b7ae3ec9d9aa6d3b6f824c00
Biruk Ephrem <birukeph@gmail.com>
2022-06-22 15:57:29 +0300

    task

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        temp = int(value)
-        print("{:d}".format(temp))
+        print("{:d}".format(int(value)))
         return True
     except ValueError:
         return False
