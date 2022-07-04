a494b22a5020161558b0a4ce7eaddf4c16b6c35a
Biruk Ephrem <birukeph@gmail.com>
2022-07-04 22:35:55 +0300

    doc-ed

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""0-square"""
+
+
 class Square:
+    """The square class"""
+
     def __init__(self):
+        """__init__ method"""
         pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""1-square"""
+
+
 class Square:
+    """The square class"""
+
     def __init__(self, size):
+        """__init__"""
         self.__size = size
