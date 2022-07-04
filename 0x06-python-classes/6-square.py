b958c5d9a064a131b2ca19489933fc4c3c848022
Biruk Ephrem <birukeph@gmail.com>
2022-07-04 23:16:38 +0300

    almost

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""6-square"""
+
+
+class Square:
+    """The square class"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """__init__"""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def my_print(self):
+        """my_print"""
+        if (self.__size == 0):
+            print()
+            return
+
+        for i in range(self.__position[1]):
+            print()
+
+        for i in range(self.__size):
+            [print(' ', end='') for j in range(self.__position[0])]
+            [print('#', end='') for j in range(self.__size)]
+            print()
+
+    def area(self):
+        """area"""
+        return self.__size * self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, position):
+        if type(position) is not tuple and len(position) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = position
