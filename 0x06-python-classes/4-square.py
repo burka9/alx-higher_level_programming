bac066dee21156edf000f11e8153edb72514dd8d
Biruk Ephrem <birukeph@gmail.com>
2022-07-04 22:57:17 +0300

    task

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""3-square"""
+"""4-square"""
 
 
 class Square:
@@ -7,6 +7,18 @@ class Square:
 
     def __init__(self, size=0):
         """__init__"""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
         if type(size) is not int:
             raise TypeError('size must be an integer')
         if size < 0:
