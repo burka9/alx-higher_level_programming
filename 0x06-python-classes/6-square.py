e1731f5d2c1812ce8f6289ed4b2732b4e33064f6
Biruk Ephrem <birukeph@gmail.com>
2022-07-04 23:42:56 +0300

    6th

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,6 +11,10 @@ class Square:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
+        if (type(position) is not tuple or
+                len(position) != 2 or
+                not all([type(num) is int and num >= 0 for num in position])):
+            raise TypeError('position must be a tuple of 2 positive integers')
         self.__size = size
         self.__position = position
 
@@ -26,30 +30,31 @@ class Square:
             raise ValueError('size must be >= 0')
         self.__size = size
 
-    def my_print(self):
-        """my_print"""
-        if (self.__size == 0):
-            print()
-            return
-
-        for i in range(self.__position[1]):
-            print()
-
-        for i in range(self.__size):
-            [print(' ', end='') for j in range(self.__position[0])]
-            [print('#', end='') for j in range(self.__size)]
-            print()
-
-    def area(self):
-        """area"""
-        return self.__size * self.__size
-
     @property
     def position(self):
         return self.__position
 
     @position.setter
     def position(self, position):
-        if type(position) is not tuple and len(position) != 2:
+        if (type(position) is not tuple or
+                len(position) != 2 or
+                not all([type(num) is int and num >= 0 for num in position])):
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = position
+
+    def my_print(self):
+        """my_print"""
+        if (self.__size == 0):
+            print()
+            return
+
+        [print() for i in range(self.__position[1])]
+
+        for i in range(self.__size):
+            [print(' ', end='') for j in range(self.__position[0])]
+            [print('#', end='') for k in range(self.__size)]
+            print()
+
+    def area(self):
+        """area"""
+        return self.__size * self.__size
