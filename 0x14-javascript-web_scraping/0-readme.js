ed99468f413b4b72e94b3a35902c24ebde2c9aa0
Biruk Ephrem <birukeph@gmail.com>
2022-10-19 11:46:45 +0300

    task

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // script that reads and prints the content of a file.
 
-const fs = require('fs')
+const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
-    console.log(err)
+    console.log(err);
   } else {
-    console.log(data)
+    console.log(data);
   }
-})
+});
